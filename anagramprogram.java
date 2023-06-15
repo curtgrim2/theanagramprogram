d22c89bc336622a0827275fc3d52b0b9af42237d
curtgrim2 <59848823+curtgrim2@users.noreply.github.com>
2023-06-15 16:57:35 -0400

    ANOTHER FAIL SOLUTION?!?
    More than 4 seems to be missing arrangements (?). Check if testing sites are including duplicates.

--- anagramprogram.java
+++ anagramprogram.java
@@ -18,19 +18,7 @@ public static void main(String [] args) {
         } 
 
        else{
-
-           /* NEW Plan: Create a for loop that represents each letter within the word. Encased within that
-           should be a while loop that iterates through all the possibilities with the 
-           next couple of letters thats not the one of focus. Noted that with each FOCUSED 
-           letter, we should loop it to the front and procede to the while loop from there.(TOTAL LETTERS - 1) . */
-
             int combocounter = 0;
-
-
-            /*NEW PLAN: Create a loop that switches a certain character with the next. Once that character
-             * reaches the end, start with the 2nd (current/thewordagain?) character. Maybe this is encased
-             * within a while loop 
-             */
 
            for(int iterate = 0; iterate < input.length(); iterate++){
 
@@ -39,8 +27,8 @@ public static void main(String [] args) {
            thewordagain[0] = thewordagain[iterate];
            thewordagain[iterate] = temp;
 
-          System.out.print("Next Main Change: ");
-          System.out.println(thewordagain);
+         // System.out.print("Next Main Change: ");
+         // System.out.println(thewordagain);
 
             String convert = String.valueOf(thewordagain);
 
@@ -78,112 +66,88 @@ public static void main(String [] args) {
 
 
 
-/* 
-           for(int position = 1; position<input.length(); position++){
-                System.out.println("And now we do...");
 
-                while(position + 1 < input.length()){
-                    temp = tempchararr[position + 1];
-                    tempchararr[position + 1] = tempchararr[position];
-                    tempchararr[position] = temp;
+         }
+
+          
+//LETS TAKE IT BACK 
+thewordagain = input.toCharArray();
+char [] prebackwards = input.toCharArray();
+int y = input.length()-1;
+for(int x=0; x<input.length();x++){
+    prebackwards[x] = thewordagain[y-x];
+    //y--;
+} //RECREATE THIS WITH(?)
+
+/*
+ * 
+ * for(int x=0; x<input.length();x++){
+    prebackwards[x] = thewordagain[y];
+    y--;
+} 
+ * 
+ */
+
+String backwards = String.valueOf(prebackwards);
+thewordagain = backwards.toCharArray();
+
+System.out.println("LETS TAKE IT BACK: " + backwards);
+
+
+  for(int iterate = 0; iterate < input.length(); iterate++){
+
+            thewordagain = backwards.toCharArray();
+           char temp = thewordagain[0];
+           thewordagain[0] = thewordagain[iterate];
+           thewordagain[iterate] = temp;
+
+          System.out.print("Next Main Change: ");
+          System.out.println(thewordagain);
+
+            String convert = String.valueOf(thewordagain);
+
+            for(int position = 1; position <input.length(); position++){
+                
+                thewordagain = convert.toCharArray();
+                int position2 = position;
+                int positionhelp = 1;
+                System.out.println("New Letter Focus");
+                while(positionhelp +1 < input.length()){
+
+                    if(position + 1 >= input.length()){
+                            position = 1;
+                    }
+
+          
+                    temp = thewordagain[position + 1];
+                    thewordagain[position + 1] = thewordagain[position];
+                    thewordagain[position] = temp;
+                    position++;
+                    positionhelp++;
+
                     
+
                     if(!possibilities.contains(String.valueOf(thewordagain))){
                     possibilities.add(String.valueOf(thewordagain));
                     
                 }
 
-                    System.out.println(tempchararr);
-                    position++;
-                    
+                    System.out.println(thewordagain);
                 }
-           }          */
-         }
-
-          
-//LETS TAKE IT BACK 
-
-/* 
-            char [] prebackwards = input.toCharArray();
-           int y = input.length()-1;
-           for(int x = 0; x< input.length(); x++){
-                prebackwards[x] = theword[y - x];
-           }
-
-           String backwards = String.valueOf(prebackwards);
-           thewordagain = backwards.toCharArray();
-           theword = backwards.toCharArray();
-
-           for(int iterate = 0; iterate < input.length(); iterate++){     
-            thewordagain = input.toCharArray();
-            char temp = thewordagain[0];
-            thewordagain[0] = thewordagain[iterate];
-            thewordagain[iterate] = temp;
-            combocounter++;
-           System.out.println(thewordagain);
-
-           char [] tempchararr = thewordagain;
-
-           System.out.print("(BACK) NEW ITERATION: ");
-
-           for(int position = 1; position<input.length(); position++){
-                tempchararr = thewordagain;
-
-                while(position + 1 < input.length()){
-                    temp = tempchararr[position + 1];
-                    tempchararr[position + 1] = tempchararr[position];
-                    tempchararr[position] = temp;
-                    
-                    System.out.println(tempchararr);
-                    position++;
-                    combocounter++;
-                }
-           }     
-        } */
-
-        //System.out.println("Combo counter: " + combocounter);
-
-           //REVERSE THE WORD
-
-           /* 
-           char [] prebackwards = input.toCharArray();
-           int y = input.length()-1;
-           for(int x = 0; x< input.length(); x++){
-                prebackwards[x] = theword[y - x];
-           }
-
-           String backwards = String.valueOf(prebackwards);
-           thewordagain = backwards.toCharArray();
-           theword = backwards.toCharArray();
-
-
-             for(int iterate = 0; iterate < input.length(); iterate++){
-            thewordagain = backwards.toCharArray();
-            int iterate2 = 0;
-            for(int position = iterate + 1; iterate2 <= input.length()-1; iterate2++){
-                if(position +1 > input.length()){                    
-                    char tempchar = thewordagain[position-1];
-                    thewordagain[position-1] = thewordagain[0];
-                    thewordagain[0] = tempchar;
-                    position=1;
-                }
-
-            else{
-                char tempchar = thewordagain[position];        
-                thewordagain[position] = theword[iterate];
-                thewordagain[position - 1] = tempchar;
-
-                if(!possibilities.contains(String.valueOf(thewordagain))){
-                    possibilities.add(String.valueOf(thewordagain));
-                }       
-                position++;
-                }
+                position = position2;
                 
             }
-           } */
+
+
+
+
+         }
+
+
 
            System.out.println("Here are all the listed possibilities (A total of " + possibilities.size() + "): ");
            for(int x = 0; x<possibilities.size(); x++){
-             // System.out.println(possibilities.get(x));
+             //System.out.println(possibilities.get(x));
            }
 
        }
