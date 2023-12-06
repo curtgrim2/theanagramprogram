91aaab2811a2e8c1f22758d8b1f27d7c3df48bfd
curtgrim2 <59848823+curtgrim2@users.noreply.github.com>
2023-12-06 14:57:36 -0500

    Fixed problem with len()

--- anagramprogram.py
+++ anagramprogram.py
@@ -1,6 +1,3 @@
-#print("The Ananagram Program...Hi!")
-theword = input("Please type in the word in question:")
-
 '''How this should be done: I'm thinking about using an API to connect and see what words can 
 be created with each distinctive char. 
 
@@ -10,30 +7,22 @@ see what word can be outputted.
 The first version of this program will probably just be outputting the rearranged letters and 
 making sure that every order is accounted for (from there apply it in a arraylist/vector as long as
 the data structure is dynamic.)'''
+def printresult(printtheword):
+    return ''.join(printtheword)
+
+def letsgotowork(theword,startindex,endindex):
+    if startindex == endindex:
+        print(printresult(theword))
+    else:
+        for iterator in range(startindex,endindex):
+            theword[startindex],theword[iterator]=theword[iterator],theword[startindex]
+            letsgotowork(theword,startindex+1,endindex)
+            theword[iterator],theword[startindex]=theword[startindex],theword[iterator]
 
 
-#Create if statements that checks whether there are any numbers in the input
-nonumbersallowed = any(char.isdigit() for char in theword)
+print('The Anagram Program...HI!')
+userinput = input("Please type in the word in question: ")
+endindex = len(userinput)
+theword = list(userinput)
+letsgotowork(theword,0,endindex)
 
-
-if(nonumbersallowed):
-    print("NO NUMBERS ALLOWED.")
-else:
-    thewordagain = ""
-    for x in theword:
-        if theword.index(x) == 0:
-            thewordagain[len(theword)] = theword
-            print("New variation: " + thewordagain)
-
-            #print("The first original char: " + x)
-        '''else:
-            thewordagain[theword.index(x)-1]=theword[theword.index(x)]
-            print("New variation: " + thewordagain)
-            #print(theword.index(x))
-            #print("Char: " + x) '''
-
-
-
-    print("These are all the possibilties:")
-
-    #print("Words that can be created with " + theword)
