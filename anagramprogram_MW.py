bdb96751b79045bd3101e1c72ba3f0b024ee265b
curtgrim2 <59848823+curtgrim2@users.noreply.github.com>
2024-01-03 10:16:38 -0500

    Cleaner Code

--- anagramprogram_MW.py
+++ anagramprogram_MW.py
@@ -5,9 +5,8 @@ import os
 import unittest
 #from wordnik.swagger import *
 #from wordnik import WordApi, swagger
-#global allcombos
+
 allcombos=0
-#global correctcombos
 correctcombos=0
 
 def printresult(printtheword):
@@ -22,23 +21,18 @@ def check_word_existence(word,api_key='f25abb33-a3be-4049-8767-f3b871339adc'):
         response = requests.get(api_url)
         if response.status_code == 200:
             confirm = response.json()
-            #if confirm:
-                #define = confirm[0].get('def', {}).get('{bc}', '')
-                    
+            #define = confirm[0].get('def', {}).get('{bc}', '')                   
             if confirm and isinstance(confirm, list) and 'meta' in confirm[0]:
-                metadata = confirm[0]['meta']
-                #print(f"{printresult(word)} has metadata:")
+                #metadata = confirm[0]['meta']
                 print(f"{printresult(word)} is a word")
                 correctcombos=correctcombos+1
                 allcombos=allcombos+1
-
             else:
                 allcombos=allcombos+1
-                #print("No metadata dude")
 
-                    #print(printresult(word)+":IS A WORD")
-                    #print(confirm[0]['def'][0].get('{bc}', ''))
-                    #print(f"{i}. {definition['def'][0]}")
+                #print(printresult(word)+":IS A WORD")
+                #print(confirm[0]['def'][0].get('{bc}', ''))
+                #print(f"{i}. {definition['def'][0]}")
                 #print(printresult(word)+":IS A WORD")
                 #print("Definition: "+ define)
 
@@ -69,33 +63,3 @@ allcombos=0'''
 letsgotowork(theword,0,endindex)
 print(f"TOTAL LETTER COMBINATIONS: {allcombos}")   
 print(f"ACTUAL WORDS FORMED: {correctcombos}")
-
-
-'''apikey='f25abb33-a3be-4049-8767-f3b871339adc'
-word='yrc'
-apiurl = f'https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={apikey}'
-response = requests.get(apiurl)
-if response.status_code == 200:
-    confirm=response.json
-    if check_word_existence(word):
-        print(word + " is a word.")
-    else:
-        print("NOT A WORD")   
-#letsgotowork(theword,0,endindex)'''
-
-#Rest is invalid
-
-
-'''
-def checkwordexistence(checkword):
-   apikey='f25abb33-a3be-4049-8767-f3b871339adc'
-   apiurl = f'https://www.dictionaryapi.com/api/v3/references/collegiate/json/{checkword}?key={apikey}'
-   response = requests.get(apiurl)
-   if response.status_code==200:
-    confirm=response.json
-    if bool(confirm):
-        print(printresult(checkword))
-    else:
-        print("INVALID WORD: " + printresult(checkword))
-'''
-
